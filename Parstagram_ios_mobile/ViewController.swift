6392a6dbd0ddc36d7f92e080b73bd282e215de39
ldiima <dima@minerva.kgi.edu>
2021-03-19 19:50:53 -0700

    Initial Commit

---
+++ Parstagram_ios_mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram_ios_mobile
+//
+//  Created by Dima Lkhagvatogtokh on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
